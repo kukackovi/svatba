64747a0a70c81db62fc48eb997bb1f94de3a3afc
David Kukačka <kukacdav@gmail.com>
2023-06-03 19:07:56 +0200

    Fixing grammar error

--- src/comp/final/LocationPage.tsx
+++ src/comp/final/LocationPage.tsx
@@ -9,11 +9,11 @@ const LocationPage = () => {
         <Col sm={12} md={6}>
             <h3>Termín</h3>
             <p>Svatba se koná 4.7. (<strong>pracovní úterý</strong>) a na místě je možné přespat.</p>
-            <p>Přijeďte mezi 11:00 a 12:00, ať máte čas si vklidu dát pivo. Ve 13:00 začíná obřad.</p>
+            <p>Přijeďte mezi 11:00 a 12:00, ať máte čas si v klidu dát pivo. Ve 13:00 začíná obřad.</p>
         </Col>
         <Col sm={12} md={6}>
             <h3>Místo</h3>
-            <p>Statek Hojšín poblíž Votice.</p>
+            <p>Statek Hojšín poblíž Votic.</p>
             <p>GPS: 49.5833339N, 14.7145836E</p>
         </Col>
 
